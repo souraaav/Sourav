7a66b1827cb9c3ee81fca8c6ea247c85132ea908
souraaav <sujitc079@gmail.com>
2025-07-02 16:18:55 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// Function to change the background color of the body when the button is clicked
-document.getElementById("changeColorButton").addEventListener("click", function() {
-    const colors = ["#FF5733", "#33FF57", "#3357FF", "#F4F4F4", "#FFC300"];
-    const randomColor = colors[Math.floor(Math.random() * colors.length)];
-    document.body.style.backgroundColor = randomColor;
+document.getElementById('toggle-theme').addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
 });
